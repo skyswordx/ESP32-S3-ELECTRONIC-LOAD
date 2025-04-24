c877521f4624edcce5e3fd400f998519991b8715
skyswordx <1140527828@qq.com>
2025-04-25 00:05:46 +0800

    细化宏定义

--- lib/our_tasks_config/our_tasks_config.hpp
+++ lib/our_tasks_config/our_tasks_config.hpp
@@ -24,7 +24,11 @@
 #define USE_PID_CONTROLLER 1
 //     #define USE_CURRENT_OPEN_LOOP_TEST 1
 #define USE_IIC_DEVICE 1 // 是否使用 IIC 设备
-    // #define USE_INA226_MODULE 1 // 是否使用 INA226 模块
+    #define USE_INA226 1
+    #define USE_MCP4725 1
+    //#define USE_INA226_MODULE 1 // 是否使用 INA226 模块
+    #define USE_LOAD_RATE_TESTING
+
 
 #define USE_LCD_DISPLAY 1  // 是否使用 LCD 显示屏
 #define USE_ENCODER1 1 // 是否使用编码器 1
@@ -49,90 +53,97 @@ extern BaseType_t debug_flag1;
 extern BaseType_t debug_flag2;
 
 
-/*********************************** Current Measurement Setup *********************/
-#ifdef USE_INA226_MODULE
-    #define RSHUNT 0.010 // 10mΩ 电阻
-    #define CURRENT_LSB_mA 0.05 // 电流 LSB 值
-    #define CURRENT_OFFSET_mA -0.525 // 校准程序串口输出的电流偏置
-    #define BUS_V_DMM 6.013 // DMM 数字万用表测量的电压
-    #define BUS_V_SERIAL 6.109 // 校准程序测量的电压
-    #define BUS_V_SCALING_E4 (10000 / BUS_V_SERIAL * BUS_V_DMM) // 电压缩放系数
-#else // 启用功率板
-    #define RSHUNT 0.020 // 20mΩ 电阻
-    #define CURRENT_LSB_mA 0.05 // 电流 LSB 值
-    #define CURRENT_OFFSET_mA -0.525 // 校准程序串口输出的电流偏置
-    #define BUS_V_DMM 6.013 // DMM 数字万用表测量的电压
-    #define BUS_V_SERIAL 6.109 // 校准程序测量的电压
-    #define BUS_V_SCALING_E4 (10000 / BUS_V_SERIAL * BUS_V_DMM) // 电压缩放系数
-#endif // USE_INA226_MODULE
-
-#ifdef USE_IIC_DEVICE // 启用电子负载测试
-    // #define DAC_BIAS_CURRENT_mA 40  
-    #define TESING_MAX_CURRENT_A 1.00
-    #define TESING_MIN_CURRENT_A 0.1
-    #define THRESHOLD_A 0.5
-    extern BaseType_t testing_load_flag;
-    double from_set_current2voltage_V( double set_current_A); // 在旋转编码器任务 (get_encoder1_data_task) 中使用
-
-    extern SemaphoreHandle_t  load_testing_xBinarySemaphore;
-    void get_load_changing_rate_task(void *pvParameters); // 测量负载调整率任务函数
-
-#endif 
-
-/****************************** OUTPUT CAlibration *******************************/
-
-#ifdef USE_OUTPUT_CALIBRATION // 启用输出校准
-        void output_data_collection_task(void *pvParameters); // 输出校准采集任务函数
-        
-        #define OUTPUT_CALIBRATION  50 // 输出校准测试数据长度
-
-        extern SemaphoreHandle_t output_calibration_xBinarySemaphore; // 输出测试二值信号量
-        extern double targ_data[OUTPUT_CALIBRATION]; // 输出测试数据-目标值
-        extern double real_data[OUTPUT_CALIBRATION]; // 输出测试数据-实际值
-        
-        #define OUTPUT_CALIB_TIME_ms  10000
-
-        #define USE_BUTTON 1
-        #define USE_BUTTON4 1 // DAC OUTPUT MAX
-        #define USE_BUTTON3 1 // DAC OUTPUT MIN
-    #endif // USE_OUTPUT_CALIBRATION 
-
-/*********************************** PID Setup ***********************************/
+/*********************************** Current PID Setup ***********************************/
 // 包含自定义的 PID 控制器类和 VOFA 下位机
 #ifdef USE_PID_CONTROLLER
     #include "our_pid_controller.hpp"
     // #include "our_vofa_debuger.hpp"
+
     extern PID_controller_t<double> current_ctrl;
 
     #define DAC_OUTPUT_V_MAX 5.0 
     #define DAC_OUTPUT_V_MIN 0.0
     // #define CURRENT_TASK_KP 0.0022226816 // 电流控制器比例系数
     // #define CURRENT_TASK_KI 0.00 // 电流控制器积分系数
-
+            
     #define CURRENT_TASK_KP 0.001 // 电流控制器比例系数
     #define CURRENT_TASK_KI 0.00 // 电流控制器积分系数
     #define CURRENT_TASK_KD 0.001 // 电流控制器微分系数
-
+            
     void set_current_task(void *pvParameters); // 设置电流任务函数
-
+            
     #ifdef USE_CURRENT_OPEN_LOOP_TEST
         void open_loop_data_collection_task(void *pvParameters); // 开环数据采集任务函数
-        
-
+                    
+            
         #define OPEN_LOOP_TEST_LENGTH  50 // 开环测试数据长度
-
+            
         extern SemaphoreHandle_t open_loop_test_xBinarySemaphore; // 开环测试二值信号量
         extern double pv_data[OPEN_LOOP_TEST_LENGTH]; // 开环测试数据
         extern double op_data[OPEN_LOOP_TEST_LENGTH]; // 开环测试数据
-        
+                    
         #define OPEN_LOOP_T1_ms  10
         #define OPEN_LOOP_T2_ms  40
-
+            
         #define USE_BUTTON 1
         #define USE_BUTTON4 1 // DAC OUTPUT MAX
         #define USE_BUTTON3 1 // DAC OUTPUT MIN
-    #endif // USE_CURRENT_OPEN_LOOP_TEST
+    #endif // USE_CURRENT_OPEN_LOOP_TEST 
+            
+
 #endif
+
+/*********************************** Current Measurement Setup *********************/
+#ifdef USE_IIC_DEVICE // 启用电子负载测试
+    #ifdef USE_INA226_MODULE
+        #define RSHUNT 0.010 // 10mΩ 电阻
+        #define CURRENT_LSB_mA 0.05 // 电流 LSB 值
+        #define CURRENT_OFFSET_mA -0.525 // 校准程序串口输出的电流偏置
+        #define BUS_V_DMM 6.013 // DMM 数字万用表测量的电压
+        #define BUS_V_SERIAL 6.109 // 校准程序测量的电压
+        #define BUS_V_SCALING_E4 (10000 / BUS_V_SERIAL * BUS_V_DMM) // 电压缩放系数
+    #else // 启用功率板
+        #define RSHUNT 0.020 // 20mΩ 电阻
+        #define CURRENT_LSB_mA 0.05 // 电流 LSB 值
+        #define CURRENT_OFFSET_mA -0.525 // 校准程序串口输出的电流偏置
+        #define BUS_V_DMM 6.013 // DMM 数字万用表测量的电压
+        #define BUS_V_SERIAL 6.109 // 校准程序测量的电压
+        #define BUS_V_SCALING_E4 (10000 / BUS_V_SERIAL * BUS_V_DMM) // 电压缩放系数
+    #endif // USE_INA226_MODULE
+
+    #ifdef USE_LOAD_RATE_TESTING
+        // #define DAC_BIAS_CURRENT_mA 40  
+        #define TESING_MAX_CURRENT_A 1.00
+        #define TESING_MIN_CURRENT_A 0.1
+        #define THRESHOLD_A 0.5
+        extern BaseType_t testing_load_flag;
+        double from_set_current2voltage_V( double set_current_A); // 在旋转编码器任务 (get_encoder1_data_task) 中使用
+
+        extern SemaphoreHandle_t  load_testing_xBinarySemaphore;
+        void get_load_changing_rate_task(void *pvParameters); // 测量负载调整率任务函数
+
+    #endif 
+#endif // USE_IIC_DEVICE
+
+/****************************** OUTPUT CAlibration *******************************/
+
+#ifdef USE_OUTPUT_CALIBRATION // 启用输出校准
+     void output_data_collection_task(void *pvParameters); // 输出校准采集任务函数
+        
+    #define OUTPUT_CALIBRATION  50 // 输出校准测试数据长度
+
+    extern SemaphoreHandle_t output_calibration_xBinarySemaphore; // 输出测试二值信号量
+    extern double targ_data[OUTPUT_CALIBRATION]; // 输出测试数据-目标值
+    extern double real_data[OUTPUT_CALIBRATION]; // 输出测试数据-实际值
+        
+    #define OUTPUT_CALIB_TIME_ms  10000
+
+    #define USE_BUTTON 1
+    #define USE_BUTTON4 1 // DAC OUTPUT MAX
+    #define USE_BUTTON3 1 // DAC OUTPUT MIN
+#endif // USE_OUTPUT_CALIBRATION 
+
+
 
 
 
@@ -154,16 +165,38 @@ extern BaseType_t debug_flag2;
     #define IIC_SDA 4
     #define IIC_SCL 5
 
-    #include "INA226.h"
-    extern INA226 INA226_device; // INA226 电流传感器对象
-    #include "MCP4725.h"
-    extern MCP4725 MCP4725_device; // MCP4725 DAC 芯片对象
+    
+    #ifdef  USE_INA226_MODULE
+        #include "INA226.h"
+        extern INA226 INA226_device; // INA226 电流传感器对象
 
-    extern const double current_calibration_A; // 电流校准系数A
-    extern const double current_calibration_B; // 电流校准系数B
-    extern const double voltage_calibration_A; // 电压校准系数A
-    extern const double voltage_calibration_B; // 电压校准系数B
-    void get_ina226_data_task(void *pvParameters); // 获取 INA226 数据的任务函数
+        extern const double current_calibration_A; // 电流校准系数A
+        extern const double current_calibration_B; // 电流校准系数B
+        extern const double voltage_calibration_A; // 电压校准系数A
+        extern const double voltage_calibration_B; // 电压校准系数B
+        void get_ina226_data_task(void *pvParameters); // 获取 INA226 数据的任务函数
+    #else
+        #include "INA226.h"
+        extern INA226 INA226_device; // INA226 电流传感器对象
+
+        extern const double current_calibration_A; // 电流校准系数A
+        extern const double current_calibration_B; // 电流校准系数B
+        extern const double voltage_calibration_A; // 电压校准系数A
+        extern const double voltage_calibration_B; // 电压校准系数B
+        void get_ina226_data_task(void *pvParameters); // 获取 INA226 数据的任务函数
+    #endif
+
+    #ifdef  USE_MCP4725
+        #include "MCP4725.h"
+        extern MCP4725 MCP4725_device; // MCP4725 DAC 芯片对象
+
+        #ifdef USE_VOLTAGE_PROTECTION
+            extern SemaphoreHandle_t over_voltage_protection_xBinarySemaphore; // 过压保护二值信号量
+            void over_voltage_protection_task(void *pvParameters); // 过压保护任务函数
+        #endif // USE_VOLTAGE_PROTECTION
+    #endif // USE_MCP4725
+    
+    
 #endif // USE_IIC_DEVICE
 
 
@@ -179,23 +212,24 @@ extern BaseType_t debug_flag2;
     #ifdef USE_BUTTON1
         extern GPIO_button_handler_t button1; 
     #endif // USE_BUTTON1
+
     #ifdef USE_BUTTON2
         extern GPIO_button_handler_t button2;
     #endif // USE_BUTTON2
+    
     #ifdef USE_BUTTON3
         extern GPIO_button_handler_t button3;
     #endif // USE_BUTTON3
+    
     #ifdef USE_BUTTON4
         extern GPIO_button_handler_t button4;
     #endif // USE_BUTTON4
+    
     #ifdef USE_ENCODER1
         extern GPIO_button_handler_t encoder1_button; // 旋转编码器的按键引脚
     #endif // USE_ENCODER1
 
-
     extern SemaphoreHandle_t button_xBinarySemaphore; // 按键二值信号量
-
-
     void button_handler_task(void *pvParameters); // 按键处理任务
 #endif // USE_BUTTON
 
@@ -220,11 +254,6 @@ extern BaseType_t debug_flag2;
     extern SemaphoreHandle_t gui_xMutex;  // gui 互斥锁句柄，LVGL 线程不安全，需要加锁
 #endif // USE_LCD_DISPLAY
 /************************************* RTOS-SetUP ***********************************/
-
-#ifdef USE_VOLTAGE_PROTECTION
-    extern SemaphoreHandle_t over_voltage_protection_xBinarySemaphore; // 过压保护二值信号量
-    void over_voltage_protection_task(void *pvParameters); // 过压保护任务函数
-#endif // USE_VOLTAGE_PROTECTION
 
 
 
